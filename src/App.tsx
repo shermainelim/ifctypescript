b0a19e3149f393b737c3e68b1576ac01e83d5502
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-03-23 14:27:55 +0800

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { BuildingViewer } from './components/building-viewer';
+import { LoginForm } from './components/login-form';
+import { MapViewer } from './components/map-viewer';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Router>
+<Routes>
+  <Route path="/building" element={<BuildingViewer/>}/>
+  <Route path="/map" element={<MapViewer/>}/>
+  <Route path="/login" element={<LoginForm/>}/>
+  <Route path="/" element={<LoginForm/>}/>
+</Routes>
+    </Router>
   );
 }
 
---
+++ src/components/login-form.tsx
@@ -0,0 +1,5 @@
+import {FC} from "react";
+
+export const LoginForm: FC =() =>{
+    return <h1>Hello Login Form Viewer ! </h1>
+}
